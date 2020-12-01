6639eff03982dc1570d2a4e5ca3d43f02138b695
yoyo <yoyo.zhou@cybex.io>
2020-12-01 21:56:13 +0800

    version 0.1.3: support multicall

--- lib/src/contracts.dart
+++ lib/src/contracts.dart
@@ -3,6 +3,7 @@ library ethereum_codec.contracts;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:eth_abi_codec/eth_abi_codec.dart';
 
@@ -43,6 +44,12 @@ class AddressConfig {
     }
     return configs.firstWhere(
       (element) => element.address == addr,
+      orElse: () => null);
+  }
+
+  ContractConfig getContractConfigBySymbol(String symbol) {
+    return configs.firstWhere(
+      (element) => element.symbol == symbol,
       orElse: () => null);
   }
 
@@ -121,14 +128,21 @@ ContractConfig getContractConfigByAddress(String address) {
   return AddressConfig.instance.getContractConfigByAddress(address);
 }
 
+/// Returns the [ContractConfig] for required contract symbol
+/// 
+/// If no matching contract found, return null
+ContractConfig getContractConfigBySymbol(String symbol) {
+  return AddressConfig.instance.getContractConfigBySymbol(symbol);
+}
+
 ContractABI getContractABIByType(String type) {
   return AddressConfig.instance.getContractABIByType(type);
 }
 
-String getContractCallPayload(ContractABI abi, String method, Map<String, dynamic> params) {
+Uint8List getContractCallPayload(ContractABI abi, String method, Map<String, dynamic> params) {
   var call = ContractCall(method);
   params.forEach((key, value) {call.setCallParam(key, value);});
-  return hex.encode(call.toBinary(abi));
+  return call.toBinary(abi);
 }
 
 typedef Future<String> ethRpcCall(String to, String data);
@@ -138,7 +152,7 @@ Future<Map<String, dynamic>> callContractByAbi(
   String method,
   Map<String, dynamic> params,
   ethRpcCall call) async {
-  var payload = getContractCallPayload(abi, method, params);
+  var payload = hex.encode(getContractCallPayload(abi, method, params));
   var result = await call(address, '0x' + payload);
   if(result.startsWith('0x'))
     result = result.substring(2);
@@ -161,4 +175,73 @@ Future<Map<String, dynamic>> callContract(
   }
   var abi = getContractABIByType(cfg.type);
   return callContractByAbi(abi, address, method, params, call);
+}
+
+/// Calls aggregate function of multicall contract, return a list of data
+/// 
+/// Pass a list of request
+/// request format:
+/// 1. for configured contract: [contract address, method, params]
+/// 2. for unconfigured contract: [abi type, contract address, method, params]
+/// 
+/// Return a list of results
+Future<List<Map<String, dynamic>>> aggregateCallContract(
+  List<List<dynamic>> args,
+  ethRpcCall call) async {
+  var multicallCfg = getContractConfigBySymbol('MULTICALL');
+  if(multicallCfg == null) {
+    throw Exception('MULTICALL contract not configured, can not use aggregate call');
+  }
+
+  var multicallABI = getContractABIByType(multicallCfg.type);
+  List<List<dynamic>> callArgs = [];
+  List<ContractABI> callAbis = [];
+  List<String> callMethods = [];
+
+  args.forEach((call) {
+    if(call.length != 3 && call.length != 4) {
+      throw Exception("Invalid call format");
+    }
+
+    String contractType, method, address;
+    Map<String, dynamic> params;
+    if(call.length == 3) {
+      var cfg = getContractConfigByAddress(call[0]);
+      if(cfg == null) {
+        throw Exception("Unconfigured contract address ${call[0]}");
+      }
+      contractType = cfg.type;
+      address = call[0];
+      method = call[1];
+      params = call[2];
+    } else {
+      contractType = call[0];
+      address = call[1];
+      method = call[2];
+      params = call[3];
+    }
+
+    if(address.startsWith('0x'))
+      address = address.substring(2);
+    var callAbi = getContractABIByType(contractType);
+    var callPayload = getContractCallPayload(callAbi, method, params);
+    callArgs.add([
+      address,
+      callPayload
+    ]);
+    callAbis.add(callAbi);
+    callMethods.add(method);
+  });
+
+  var r = await callContractByAbi(multicallABI, multicallCfg.address, 'aggregate', {'calls': callArgs}, call);
+  var returnData = r['returnData'] as List;
+  if(returnData.length != callAbis.length) {
+    throw Exception("Unmatched call and return data");
+  }
+
+  List<Map<String, dynamic>> results = [];
+  for(var i = 0; i < callAbis.length; i++) {
+    results.add(callAbis[i].decomposeResult(callMethods[i], Uint8List.fromList(returnData[i] as List<int>)));
+  }
+  return results;
 }
